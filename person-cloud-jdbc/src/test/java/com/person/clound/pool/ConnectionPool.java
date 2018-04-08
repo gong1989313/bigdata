2b0d758e257b8efeec504a5a5e63871da373595d
Administrator <Administrator@F1RU37WELLAEON0>
2018-04-08 20:40:23 +0800

    fix

---
+++ person-cloud-jdbc/src/test/java/com/person/clound/pool/ConnectionPool.java
@@ -0,0 +1,48 @@
+package com.person.clound.pool;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+public class ConnectionPool {
+	private LinkedList<Connection> pool = new LinkedList<Connection>();
+
+	public ConnectionPool(int initialSize) {
+		if (initialSize > 0) {
+			for(int i=0; i< initialSize; i++) {
+				pool.addLast(ConnectionDriver.createConnection());
+			}
+		}
+	}
+	
+	public void releaseConnection(Connection connection) {
+		if(connection != null) {
+			synchronized (pool) {
+				pool.add(connection);
+				pool.notifyAll();
+			}
+ 		}
+	}
+	
+	public Connection fetchConnection(long mills) throws InterruptedException {
+		synchronized (pool) {
+			if(mills <= 0) {
+				while(pool.isEmpty()) {
+					pool.wait();
+				}
+				return pool.removeFirst();
+			} else {
+				long future = System.currentTimeMillis() + mills;
+				long remaining = mills;
+				while(pool.isEmpty() && remaining > 0) {
+					pool.wait();
+					remaining  =  future - System.currentTimeMillis();
+				}
+				Connection result = null;
+				if(!pool.isEmpty()) {
+					result = pool.removeFirst();
+				}
+				return result;
+			}
+		}
+	}
+}
