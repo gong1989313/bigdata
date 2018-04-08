6d5df5e445a39957bbc74198853e3984fbc65dd2
Administrator <Administrator@F1RU37WELLAEON0>
2018-04-08 22:58:06 +0800

    fix

--- person-cloud-jdbc/src/test/java/com/person/clound/pool/thread/SimpleHttpServer.java
+++ person-cloud-jdbc/src/test/java/com/person/clound/pool/thread/SimpleHttpServer.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
