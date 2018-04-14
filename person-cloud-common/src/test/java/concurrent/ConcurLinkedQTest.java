f12798d20e66e3036abe0b7abf97b769107d65a5
Administrator <Administrator@F1RU37WELLAEON0>
2018-04-14 23:34:05 +0800

    fix

---
+++ person-cloud-common/src/test/java/concurrent/ConcurLinkedQTest.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.junit.Test;
+
+public class ConcurLinkedQTest {
+
+	@Test
+	public void test() {
+		ConcurrentLinkedQueue<String> clq = new ConcurrentLinkedQueue<String>();
+		clq.offer("a");
+		clq.offer("b");
+		clq.offer("c");
+		clq.offer("d");
+		clq.offer("e");
+	}
+
+}
